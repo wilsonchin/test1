3a38f345bb6b747d2f7600457385fa5bc3d9d007
wilsonchin <wilsonchin@yahoo.com>
2014-08-27 22:11:23 -0700

    just add something

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("line 1" + 1 + "line2");
+    }
+}
